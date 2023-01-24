f4c89522d13748e58dab4f13a1cbe0870002b97b
ANSILYAS <114913715+ansilyas@users.noreply.github.com>
2023-01-24 23:36:39 +0500

    Add files via upload

---
+++ inner and outer HTML/js/script.js
@@ -0,0 +1,14 @@
+let x=document.getElementsByTagName("span")[0];
+console.log(x)
+let y=document.getElementsByTagName("span")[0];
+console.dir(y)
+console.log(document.body.firstChild.nodeName)
+console.log(document.body.firstElementChild.nodeName)
+
+// [innerHTML]..Tag Give the tag which is inside the (Tag)
+first.innerHTML
+
+// [outerHTML] it Give complete Element 
+first.outerHTML
+
+console.log(document.body.textContent)
